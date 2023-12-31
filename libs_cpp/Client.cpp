be83ad7fd2e4c322c41288eece53343acdddf398
SadNguyen2107 <nguyentrungson217@gmail.com>
2024-01-01 00:29:40 +0700

    Change Code style

--- libs_cpp/Client.cpp
+++ libs_cpp/Client.cpp
@@ -215,7 +215,11 @@ namespace SN_Client
         while (std::getline(text_file, chunk, static_cast<char>(EOF)) && !chunk.empty())
         {
             // Synchronous write
-            std::size_t bytes_sent = boost::asio::write(*client_socket, boost::asio::buffer(chunk), error);
+            std::size_t bytes_sent = boost::asio::write(
+                *client_socket, 
+                boost::asio::buffer(chunk), 
+                error
+            );
 
             // Check Whether Error Happen
             if (!error)
